96eba0d126081cfd5e24b295e89e1fb548bba912
Jahmayn Williams <ac122317@avcol.school.nz>
2025-03-28 13:21:56 +1300

    Added raw sql insert file

---
+++ AvcolFacilityManager/SQLQueries/InsertTestFacilities.sql
@@ -0,0 +1,11 @@
+﻿INSERT INTO Facility (FacilityName, FacilityType, Capacity)
+VALUES ('Test Facility', 'Test', 100),
+('Test Facility2', 'Test', 100),
+('Test Facility3', 'Test', 100),
+('Test Facility4', 'Test', 100),
+('Test Facility5', 'Test', 100),
+('Test Facility6', 'Test', 100),
+('Test Facility7', 'Test', 100),
+('Test Facility8', 'Test', 100),
+('Test Facility9', 'Test', 100),
+('Test Facility10', 'Test', 100);
