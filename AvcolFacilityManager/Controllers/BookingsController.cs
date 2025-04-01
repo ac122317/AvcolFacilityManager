a5639d8f72f50c3115e378746f1bdc1308453350
ac122317 <ac122317@8CG8163KY4>
2025-04-02 10:00:19 +1300

    reverted the change i made in booking to show the first name on the booking. I put it back to the app users id for now because if there were 2 of the same first names then there wouldnt be a way to differentiate. This will change when i link the registered account to the booking properly so when the user logs in theyll only see their own bookings, without their name details. But for the admin, the id will be displayed

--- AvcolFacilityManager/Controllers/BookingsController.cs
+++ AvcolFacilityManager/Controllers/BookingsController.cs
@@ -49,7 +49,7 @@ namespace AvcolFacilityManager.Controllers
         // GET: Bookings/Create
         public IActionResult Create()
         {
-            ViewData["AppUserId"] = new SelectList(_context.AppUser, "Id", "FirstName");
+            ViewData["AppUserId"] = new SelectList(_context.AppUser, "Id", "Id");
             ViewData["FacilityId"] = new SelectList(_context.Facility, "FacilityId", "FacilityName");
             return View();
         }
@@ -67,7 +67,7 @@ namespace AvcolFacilityManager.Controllers
                 await _context.SaveChangesAsync();
                 return RedirectToAction(nameof(Index));
             }
-            ViewData["AppUserId"] = new SelectList(_context.AppUser, "Id", "FirstName", bookings.AppUserId);
+            ViewData["AppUserId"] = new SelectList(_context.AppUser, "Id", "Id", bookings.AppUserId);
             ViewData["FacilityId"] = new SelectList(_context.Facility, "FacilityId", "FacilityName", bookings.FacilityId);
             return View(bookings);
         }
@@ -85,7 +85,7 @@ namespace AvcolFacilityManager.Controllers
             {
                 return NotFound();
             }
-            ViewData["AppUserId"] = new SelectList(_context.AppUser, "Id", "FirstName", bookings.AppUserId);
+            ViewData["AppUserId"] = new SelectList(_context.AppUser, "Id", "Id", bookings.AppUserId);
             ViewData["FacilityId"] = new SelectList(_context.Facility, "FacilityId", "FacilityName", bookings.FacilityId);
             return View(bookings);
         }
@@ -122,7 +122,7 @@ namespace AvcolFacilityManager.Controllers
                 }
                 return RedirectToAction(nameof(Index));
             }
-            ViewData["AppUserId"] = new SelectList(_context.AppUser, "Id", "FirstName", bookings.AppUserId);
+            ViewData["AppUserId"] = new SelectList(_context.AppUser, "Id", "Id", bookings.AppUserId);
             ViewData["FacilityId"] = new SelectList(_context.Facility, "FacilityId", "FacilityName", bookings.FacilityId);
             return View(bookings);
         }
