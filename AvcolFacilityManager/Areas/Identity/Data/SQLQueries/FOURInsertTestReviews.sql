676a7b13b03fb6105cde4c9414fc66242c9cbdf6
ac122317 <ac122317@8CG8163KY4>
2025-04-04 14:13:45 +1300

    Modified  and added to my sql queries, all tables can now be populated via raw sql scripts

---
+++ AvcolFacilityManager/Areas/Identity/Data/SQLQueries/FOURInsertTestReviews.sql
@@ -0,0 +1,7 @@
+﻿INSERT INTO Reviews (BookingId, Rating, Comment, DateCreated)
+VALUES 
+(1, 5, 'Amazing facility! Super clean and well-maintained.', '2025-10-04 12:30:00'),
+(2, 5, 'Great experience! Everything was in perfect condition.', '2025-10-05 14:45:00'),
+(3, 5, 'Loved it! Booking was smooth and hassle-free.', '2025-10-06 09:15:00'),
+(4, 5, 'Fantastic service, will definitely book again!', '2025-10-07 16:00:00'),
+(5, 5, 'Outstanding! The best facility I have used so far.', '2025-10-08 18:20:00');
